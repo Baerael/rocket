2c559d63e9b8d9b975af80fbdccc5b3b7d37a9e6
Baerael <badartic@yahoo.com>
2022-12-29 16:27:23 -0700

    fixed indentation

--- server/src/api/controller/transactions.js
+++ server/src/api/controller/transactions.js
@@ -2,14 +2,14 @@ export const transfer = async (req,  res) => {
     const { students } = req.body
     let total = 0;
     for( const student of students ) {
-    student.total = student.transactions.reduce( ( a, b ) => a + b, 0 );
-    total += student.total;
+        student.total = student.transactions.reduce( ( a, b ) => a + b, 0 );
+        total += student.total;
     }
 
     for( const student of students ) {
-    student.owes = total / students.length - student.total;
-    student.pays = [];
-    student.gets = [];
+        student.owes = total / students.length - student.total;
+        student.pays = [];
+        student.gets = [];
     }
 
     for( const student_a of students ) {
